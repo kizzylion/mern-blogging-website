ffb622fb5a93ee415d1b1ce7997beb449131139b
kizzylion <kztchm@gmail.com>
2025-06-27 18:13:04 -0300

    ADD BLOG EDITOR BANNER IN FRONTEND

--- blogging website - frontend/src/components/blog-editor.component.jsx
+++ blogging website - frontend/src/components/blog-editor.component.jsx
@@ -1,7 +1,15 @@
 import { Link } from "react-router-dom";
 import logo from "../imgs/logo.png";
+import AnimationWrapper from "../common/page-animation";
+import defaultBanner from "../imgs/blog banner.png";
 
 const BlogEditor = () => {
+  const handleBannerUpload = (e) => {
+    let img = e.target.files[0];
+
+    console.log(img);
+  };
+
   return (
     <>
       <nav className="navbar">
@@ -15,6 +23,25 @@ const BlogEditor = () => {
           <button className="btn-light py-2">Save Draft</button>
         </div>
       </nav>
+
+      <AnimationWrapper>
+        <section>
+          <div className="mx-auto max-w-[900px] w-full">
+            <div className="relative aspect-video bg-white border-4 border-grey hover:opacity-80">
+              <label htmlFor="uploadBanner">
+                <img src={defaultBanner} alt="" className="z-20" />
+                <input
+                  id="uploadBanner"
+                  type="file"
+                  accept=".png, .jpg, .jpeg"
+                  hidden
+                  onChange={handleBannerUpload}
+                />
+              </label>
+            </div>
+          </div>
+        </section>
+      </AnimationWrapper>
     </>
   );
 };
