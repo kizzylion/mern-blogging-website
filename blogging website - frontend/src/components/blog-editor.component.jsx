b88e043ff78ce2d41ccd770bd3ccf499ad148282
kizzylion <kztchm@gmail.com>
2025-06-28 02:23:23 -0300

    ADD BLOG EDITOR WITH TOOLS LIKE HEADER, IMAGE, LINKS ETC

--- blogging website - frontend/src/components/blog-editor.component.jsx
+++ blogging website - frontend/src/components/blog-editor.component.jsx
@@ -3,9 +3,11 @@ import logo from "../imgs/logo.png";
 import AnimationWrapper from "../common/page-animation";
 import defaultBanner from "../imgs/blog banner.png";
 import { uploadImage } from "../common/aws";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { Toaster, toast } from "react-hot-toast";
 import { EditorContext } from "../pages/editor.pages";
+import EditorJS from "@editorjs/editorjs";
+import { tools } from "./tools.component";
 
 const BlogEditor = () => {
   // let blogBannerRef = useRef();
@@ -13,7 +15,22 @@ const BlogEditor = () => {
     blog,
     blog: { title, banner, content, tags, des },
     setBlog,
+    textEditor,
+    setTextEditor,
   } = useContext(EditorContext);
+
+  // useEffect
+
+  useEffect(() => {
+    setTextEditor(
+      new EditorJS({
+        holderId: "textEditor",
+        data: "",
+        tools: tools,
+        placeholder: "Let's write and awesome story",
+      })
+    );
+  }, []);
 
   const handleBannerUpload = (e) => {
     let img = e.target.files[0];
@@ -45,7 +62,6 @@ const BlogEditor = () => {
   };
 
   const handleTitleChange = (e) => {
-    console.log(e);
     let input = e.target;
 
     input.style.height = "auto";
@@ -105,6 +121,8 @@ const BlogEditor = () => {
             ></textarea>
 
             <hr className="w-full opacity-10 my-5" />
+
+            <div id="textEditor" className="font-gelasio"></div>
           </div>
         </section>
       </AnimationWrapper>
