e52c46a9cbd89b1cc374b03624dc562139f36939
kizzylion <kztchm@gmail.com>
2025-06-28 01:36:29 -0300

    CREATE EDITOR CONTEXT TO STORE AND USE BLOG DATA IN EDITOR COMPONENT

--- blogging website - frontend/src/components/blog-editor.component.jsx
+++ blogging website - frontend/src/components/blog-editor.component.jsx
@@ -3,11 +3,17 @@ import logo from "../imgs/logo.png";
 import AnimationWrapper from "../common/page-animation";
 import defaultBanner from "../imgs/blog banner.png";
 import { uploadImage } from "../common/aws";
-import { useRef } from "react";
+import { useContext } from "react";
 import { Toaster, toast } from "react-hot-toast";
+import { EditorContext } from "../pages/editor.pages";
 
 const BlogEditor = () => {
-  let blogBannerRef = useRef();
+  // let blogBannerRef = useRef();
+  let {
+    blog,
+    blog: { title, banner, content, tags, des },
+    setBlog,
+  } = useContext(EditorContext);
 
   const handleBannerUpload = (e) => {
     let img = e.target.files[0];
@@ -19,7 +25,9 @@ const BlogEditor = () => {
           if (url) {
             toast.dismiss(loadingToast);
             toast.success("Uploaded 👍");
-            blogBannerRef.current.src = url;
+            // blogBannerRef.current.src = url;
+
+            setBlog({ ...blog, banner: url });
           }
         })
         .catch((err) => {
@@ -29,13 +37,38 @@ const BlogEditor = () => {
     }
   };
 
+  const handleTitleKeyDown = (e) => {
+    if (e.keyCode == 13) {
+      //enter key
+      e.preventDefault();
+    }
+  };
+
+  const handleTitleChange = (e) => {
+    console.log(e);
+    let input = e.target;
+
+    input.style.height = "auto";
+    input.style.height = input.scrollHeight + "px";
+
+    setBlog({ ...blog, title: input.value });
+  };
+
+  const handleError = (e) => {
+    let img = e.target;
+
+    img.src = defaultBanner;
+  };
+
   return (
     <>
       <nav className="navbar">
         <Link to={"/"}>
           <img src={logo} alt="" className="flex-none w-10" />
         </Link>
-        <p className=" mx-md:hidden text-black line-clamp-1 w-full">New Blog</p>
+        <p className=" mx-md:hidden text-black line-clamp-1 w-full">
+          {title.length ? title : "New Blog"}
+        </p>
 
         <div className="flex gap-4 ml-auto">
           <button className="btn-dark py-2">Publish</button>
@@ -49,10 +82,11 @@ const BlogEditor = () => {
             <div className="relative aspect-video bg-white border-4 border-grey hover:opacity-80">
               <label htmlFor="uploadBanner">
                 <img
-                  ref={blogBannerRef}
-                  src={defaultBanner}
+                  // ref={blogBannerRef}
+                  src={banner}
                   alt=""
                   className="z-20"
+                  onError={handleError}
                 />
                 <input
                   id="uploadBanner"
@@ -63,6 +97,14 @@ const BlogEditor = () => {
                 />
               </label>
             </div>
+            <textarea
+              placeholder="Blog Title"
+              className="text-4xl font-medium w-full h-20 outline-none resize-none mt-10 leading-tight placeholder:opacity-40"
+              onKeyDown={handleTitleKeyDown}
+              onChange={handleTitleChange}
+            ></textarea>
+
+            <hr className="w-full opacity-10 my-5" />
           </div>
         </section>
       </AnimationWrapper>
