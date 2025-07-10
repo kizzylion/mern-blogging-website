4cb150961f52d15ca1bb9d8b83b2d5f47fc97c17
kizzylion <kztchm@gmail.com>
2025-07-09 22:19:33 -0300

    FETCH THE LATEST BLOGS FROM THE SERVER TO THE FRONTEND

--- blogging website - frontend/src/pages/home.page.jsx
+++ blogging website - frontend/src/pages/home.page.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
 import AnimationWrapper from "../common/page-animation";
 import InPageNavigation from "../components/inpage-navigation.component";
+import axios from "axios";
+import Loader from "../components/loader.component";
 
 const Homepage = () => {
+  let [blogs, setBlogs] = useState(null);
+
+  const fetchLatestBlogs = () => {
+    axios
+      .get(import.meta.env.VITE_SERVER_DOMAIN + "/latest-blogs")
+      .then(({ data }) => {
+        setBlogs(data.blogs);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    fetchLatestBlogs();
+  }, []);
   return (
     <AnimationWrapper>
       <section className="h-cover flex justify-center gap-10 ">
@@ -11,7 +30,15 @@ const Homepage = () => {
             routes={["home", "trending blogs"]}
             defaultHidden={["trending blogs"]}
           >
-            <h1>Latest Blogs Here</h1>
+            <>
+              {blogs == null ? (
+                <Loader />
+              ) : (
+                blogs.map((blog, i) => {
+                  return <h1 key={i}> {blog.title}</h1>;
+                })
+              )}
+            </>
 
             <h1>Trending Blogs</h1>
           </InPageNavigation>
